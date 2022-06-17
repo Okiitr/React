a5bd41685e44d12c62370e4f283174c8f3e0f1f3
abhijit.kumar <abhijit.kumar@dremio.com>
2022-06-17 15:12:38 +0530

    office

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
+// import Navbar from "./components/Navbar";
 
 function App() {
   return (
+    // <Navbar heading="Your web"/>
     <nav className="navbar navbar-expand-lg bg-dark-light" >
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Website</a>
+      <a className="navbar-brand" href="#">heading</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -15,7 +17,7 @@ function App() {
             <a className="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
+            <a className="nav-link" href="#">about</a>
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
