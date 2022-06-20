22785a7db68b3774c0e557a8c01513ae42fe934d
abhijit.kumar <abhijit.kumar@dremio.com>
2022-06-20 10:18:07 +0530

    dark mode app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,25 @@ import Navbar from "./components/Navbar";
 import Textarea from "./components/Textarea";
 
 function App() {
+
+  const[Mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+        setMode('dark');
+        document.body.style.backgroundColor = 'blue';
+    }
+    else {
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+    }
+};
+
   return (
     <>
-    <Navbar heading="Oman" about ="About"/>
+    <Navbar heading="Oman" about ="About" toggleMode={toggleMode}/>
     <div className="container my-3">
-    <Textarea head="Enter Text Here"/>
+    <Textarea head="Enter Text Here" mode={Mode}/>
     </div>
     </>
   );
--- my-app/src/components/Textarea.js
+++ my-app/src/components/Textarea.js
@@ -1,29 +1,29 @@
 import { useState } from "react";
-import React  from "react";
+import React from "react";
 
 
 
 export default function Textarea(props) {
-    const [text, setText] = useState(" ");
-    
-    const changeToUpper=()=>{
-        let newText=text.toUpperCase();
-        setText(newText)
-    }
-    const changeToLower=()=>{
-        let newText=text.toLowerCase();
-        setText(newText)
-    }
-    const handleOnChange=(event)=>{
-        console.log("changing state")
-        setText(event.target.value) //updating our text when we are writing on it.
-    }
+  const [text, setText] = useState(" ");
+
+  const changeToUpper = () => {
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+  const changeToLower = () => {
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+  const handleOnChange = (event) => {
+    console.log("changing state")
+    setText(event.target.value) //updating our text when we are writing on it.
+  }
   return (
-    <div>
+    <div style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
       <h2>{props.head}</h2>
-      <div className="mb-3">
+      <div className="mb-3" >
         <label htmlFor="exampleFormControlTextarea1" className="form-label"> Example textarea </label>
-        <textarea   className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1"   rows="8" ></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8" style={{ backgroundcolour: props.mode === 'dark' ? 'Grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}></textarea>
         <button className="btn btn-primary" onClick={changeToUpper} >Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={changeToLower}>Convert to lowercase</button>
       </div>
@@ -31,6 +31,10 @@ export default function Textarea(props) {
         <h2>You Text Summary</h2>
         <p>There are {text.split(" ").length} words and {text.length} characters in your text</p>
       </div>
+       <div className="container my-4">
+          <h2>Preview</h2>
+          <p>{text.length > 0 ? text : "Enter something in the above textbox to preview it here"}</p>
+        </div>
     </div>
-  );
+      );
 }
